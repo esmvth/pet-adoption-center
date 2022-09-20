8430eeebae506ef4710b5d97a707935749e0ae4e
esmvth <evizueth@outlook.com>
2022-09-19 23:56:59 -0400

    first push

---
+++ README.md
@@ -0,0 +1,10 @@
+# Adoption Center Website
+
+Author: Esmeralda Vizueth
+
+## Description
+
+A basic website for an adoption center.
+Enter using the following credentials
+    Email: person@email.com
+    Password: 123
---
+++ js/login.js
@@ -0,0 +1,14 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    if (username === "person@email.com" && password === "123") {
+        location.href="/html/dogs.html";
+        
+    }
+})
+
